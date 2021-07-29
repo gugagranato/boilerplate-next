e9af40e7bbc788d73abeec98c5e757aa4f02290c
Gustavo Granato <gustavo.granato@hotmail.com>
2021-07-29 14:46:11 -0300

    chore: add and config storybook

---
+++ .storybook/main.js
@@ -0,0 +1,11 @@
+module.exports = {
+  "stories": [
+    "../src/components/**/*.stories.mdx",
+    "../src/components/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/preset-typescript",
+    "@storybook/addon-links",
+    "@storybook/addon-essentials"
+  ]
+}
